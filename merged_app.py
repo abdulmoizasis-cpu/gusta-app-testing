60287196b65adaac1be8d9cea13569c2658477fb
abdulmoizasis-cpu <abdulmoizasis@gmail.com>
2025-09-15 14:49:30 +0300

    Update merged_app.py

--- merged_app.py
+++ merged_app.py
@@ -3,8 +3,6 @@ from streams import *
 from process_functions import *
 from process_row import *
 from concurrent.futures import ThreadPoolExecutor, as_completed
-import streamlit.components.v1 as components # Make sure to import components
-
 st.set_page_config(layout="wide")
 st.title("Agentic-flow tester")
 st.markdown("Click Run Analysis to start the tester.")
